d8f2b681ae8f08eeae5a5f8a7c2164676042f378
Matthew Hopkins <matthew.hopkins9942@gmail.com>
2023-06-08 13:15:59 +0100

    trying things

--- testingGaiaunlimited.py
+++ testingGaiaunlimited.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import numpy as np
 import pandas as pd
@@ -66,16 +67,137 @@ def plotCompleteness(G, coords=get_healpix_centers(7), map=DR3SelectionFunctionT
 
 # RVS:
     
-inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-1.3,5.1,0.4],
-            'parallax': [10,30,10], # for actual shouldn't bin in parallax, for now this has effect of reducing time
-         'mh_gspspec': [-0.9,0.9, 0.9] # for actual shouldn't bin here, just testing table join of ap worked, and if ap. or s. must be specified
-        }
+# inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-1.3,5.1,0.4],#[-2.5,5.1,0.4],
+#             'parallax': [10,30,10], # for actual shouldn't bin in parallax, for now this has effect of reducing time
+#          'mh_gspspec': [-0.1,0.1, 0.1] # for actual shouldn't bin here, just testing table join of ap worked, and if ap. or s. must be specified
+#         }
+# fname = 'test'
 
-print('starting')
-subsampleSF = subsample.SubsampleSelectionFunction(
-                subsample_query = "radial_velocity is not null",
-                file_name = 'dr3_lvl4', hplevel_and_binning = inDict)
+# inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-1.3,5.1,0.4],#[-2.5,5.1,0.4],
+#             'parallax': [10,30,10], # for actual shouldn't bin in parallax, for now this has effect of reducing time
+#          'mh_gspspec': [-0.1,0.1, 0.1] # for actual shouldn't bin here, just testing table join of ap worked, and if ap. or s. must be specified
+#         }
+# fname = 'hp5, '
+
+# inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-1.3,5.1,0.4],#[-2.5,5.1,0.4],
+#             'parallax': [10,30,10], # for actual shouldn't bin in parallax, for now this has effect of reducing time
+#          'mh_gspspec': [-0.1,0.1, 0.1] # for actual shouldn't bin here, just testing table join of ap worked, and if ap. or s. must be specified
+#         }
+# fname = 'test'
+
+# print('starting')
+# subsampleSF = subsample.SubsampleSelectionFunction(
+#                 subsample_query = "radial_velocity IS NOT NULL AND mh_gspspec IS NOT NULL",
+#                 file_name = 'lvl4', hplevel_and_binning = inDict, use_astrophysical_parameters=True)
+
+#I'll run this with SF in paper to check, and one with mh is not null,
+#and one using builti rv selection(also hp5)
+# and finally one which should cause error
+#name well, and wll be saved locally
+# would be nice to have fig 5 type plot - unclear how they made it, but 
+# probalby can make by n-weighting healpix
+#bug shouldn't cause problem if I follow paper - therefore lvl4
+
+startTime=time.time()
+
+try:
+    
+    # print('run1')
+    # inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-2.5,5.1,0.4]}
+    # fname = 'hp4_docsRange_rv'
+    # ssquery = "radial_velocity IS NOT NULL"
+    # subsampleSF = subsample.SubsampleSelectionFunction(
+    #                 subsample_query = ssquery,
+    #                 file_name = fname, hplevel_and_binning = inDict,
+    #                 use_astrophysical_parameters=False)
+    
+    print('run2')
+    inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-2.5,5.1,0.4]}
+    fname = 'hp4_docsRange_rv_mh'
+    ssquery = "radial_velocity IS NOT NULL AND mh_gspspec IS NOT NULL"
+    subsampleSF = subsample.SubsampleSelectionFunction(
+                    subsample_query = ssquery,
+                    file_name = fname, hplevel_and_binning = inDict,
+                    use_astrophysical_parameters=True)
+    
+    # print('run3')
+    # rvssf = subsample.DR3RVSSelectionFunction()
+    
+    # print('run4')
+    # inDict = {'healpix': 4,'phot_g_mean_mag': [3,20,0.2],'g_rp': [-2.5,5.1,0.4]}
+    # fname = 'hp4_docsRange_rv_mh_noAP'
+    # ssquery = "radial_velocity IS NOT NULL AND mh_gspspec IS NOT NULL"
+    # subsampleSF = subsample.SubsampleSelectionFunction(
+    #                 subsample_query = ssquery,
+    #                 file_name = fname, hplevel_and_binning = inDict,
+    #                 use_astrophysical_parameters=False)
+    
+except ConnectionResetError:
+    print(time.time()-startTime)
+
+print(time.time()-startTime)
 
 
+#explaination of how query works
+"""once filled in, it's 
+SELECT healpix_, phot_g_mean_mag_, g_rp_, COUNT(*) AS n, SUM(selection) AS k
+FROM (
+      SELECT 
+            to_integer(GAIA_HEALPIX_INDEX(4,source_id)) AS healpix_,
+            to_integer(floor((phot_g_mean_mag - 3)/0.2)) AS phot_g_mean_mag_,
+            to_integer(floor((g_rp - -1.3)/0.4)) AS g_rp_,
+            to_integer(IF_THEN_ELSE('radial_velocity IS NOT NULL 
+                                    AND mh_gspspec IS NOT NULL',1.0,0.0)) AS selection
+      FROM gaiadr3.gaia_source JOIN gaiadr3.astrophysical_parameters USING (source_id)
+      WHERE phot_g_mean_mag > 3    AND phot_g_mean_mag < 20 
+            AND        g_rp > -1.3 AND            g_rp < 5.1
+     ) AS subquery
+GROUP BY healpix_, phot_g_mean_mag_, g_rp_
 
+Firstly, FROM (...) AS subquery defines a subquery which builds a
+'temporary table' that the outer clauses draw from.
+In subquery, goes through gaiadr3.source (and ap), limited to stars in the 
+binning range, recording the bin index of each star (floor etc)
+as healpix_, phot_g_mean_mag_, and g_rp_, and recording if rv and mh are not 
+null in a column called 'selection'.
+ Result is that the 'temporary table' has row for each star in binning
+range, with the bin index in healpix_, phot_g_mean_mag_, g_rp_, and if in subsample in selection
+Outside subquery, GROUP BY means that for each combo value of  healpix_, phot_g_mean_mag_, g_rp_
+(i.e bin) counts number of rows as n and number in subsample (selection=1) as k
 
+This is why if no stars in bin it isn' carried forward. There aren't the rows to group 
+"""
+
+"""
+looks like everything working. one tha meat to fail did, and precompted rvssf looks like my version 
+slihgt differences attributed to mine being hp4, theirs hp5
+mh version timed out - could it be becasue of join to ap?
+afer how long did it time out? if current run still going at 11.15 its an hour
+can it b sped up?
+or broken up: could see where problems are
+
+Running with timer: if exactly an hour (11.35 start) then likely a tieout problem
+https://www.cosmos.esa.int/web/gaia-users/archive/faq
+says anonymous tiout is 90min, registered and logged in is 120min
+
+Depending on what is slow bit, could use source_lite and/or count(healpix_) instead of count(*)
+assuming no NULL in healPix_
+
+could possibly just do part of work in query, rest locally. Or all, on cluster
+
+slow bit is probably subquery:
+SELECT 
+      to_integer(GAIA_HEALPIX_INDEX(4,source_id)) AS healpix_,
+      to_integer(floor((phot_g_mean_mag - 3)/0.2)) AS phot_g_mean_mag_,
+      to_integer(floor((g_rp - -1.3)/0.4)) AS g_rp_,
+      to_integer(IF_THEN_ELSE('radial_velocity IS NOT NULL 
+                              AND mh_gspspec IS NOT NULL',1.0,0.0)) AS selection
+FROM gaiadr3.gaia_source JOIN gaiadr3.astrophysical_parameters USING (source_id)
+WHERE phot_g_mean_mag > 3    AND phot_g_mean_mag < 20 
+      AND        g_rp > -1.3 AND            g_rp < 5.1
+
+by advice on gaia website calculaions in query are slow, could get all data and calc bins on cluster
+
+OR do each bin as a seprate quesry! could deal with n=0 bug while I'm at it
+at 100 minutes, hasn't timed out, but last timefailed at 60ish. Unsure what's going wrong, but splitting it up will help
+"""
